3b67f3a95926bcf2cf63bf3d969b539a1ebdb41f
Wei Wang <thomas.wei.wang@gmail.com>
2012-07-02 16:10:27 -0400

    r code

---
+++ teaching/s1211q/R_demo_1.R
@@ -0,0 +1,19 @@
+## Stem-and-Leaf plot
+islands # Areas of the World' major landmasses
+stem(log10(islands))
+
+## dotplot
+install.packages("epicalc") # first install epicalc pacakge
+library(epicalc) # load the package
+data(Outbreak) # load Outbreak data set
+use(Outbreak)
+age
+dotplot(age)
+
+## histogram
+example_data <- c(67, 72, 74, 62, 56, 66, 65, 59, 61, 69, 74, 69, 66, 68, 58, 64, 66, 57, 68, 62, 59, 73, 61, 61, 57, 58, 57, 67, 81, 79, 76, 78, 74, 67, 84, 85, 79, 82, 87, 90, 87, 93, 92, 82, 80, 79, 77, 72, 65, 73, 76, 77, 76, 76, 76, 75, 78, 73, 80, 77, 83, 84, 85, 81, 84, 83, 83, 88, 92, 92, 89, 82, 73, 81, 91,  80, 81, 82, 84, 87, 85, 74, 81, 82, 86, 85, 82, 86, 88, 86, 83, 81, 81, 81, 82, 86, 85, 87, 89, 90,  90, 92, 86, 86, 82, 80, 79, 77, 79, 76, 78, 78, 77, 72, 75, 79, 81, 86, 88, 97, 94, 96, 94, 91, 92, 93, 93, 87, 84, 80, 78, 75, 73, 81, 76, 77, 71, 71, 78, 67, 76, 68, 82, 64, 71, 81, 69, 63, 70, 77,  75, 76, 68)
+hist(data, freq=TRUE)
+hist(data, freq=FALSE)
+
+## numerical summary
+summary(example_data)
