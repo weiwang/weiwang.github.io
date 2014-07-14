ff9de9647195ba84f8bcebcd874c237782a8532e
Wei Wang <thomas.wei.wang@gmail.com>
2014-07-14 19:35:23 -0400

    added r file

--- teaching/g4242.R
+++ teaching/g4242.R
@@ -17,7 +17,13 @@ legend("bottomright", legend=paste("line", 1:5), lty=5:10)
 par(op)
 
 ## save your graph to a file
+pdf("myplot.pdf")
+#
+# your plotting code here
+#
+dev.off()
 
+## use brewer palette
 install.packages(c("RColorBrewer"))
 library(RColorBrewer)
 colors <- brewer.pal(4, "Set1")
@@ -27,14 +33,31 @@ pal(5)
 plot(1:20, 1:20, col=pal(10))
 display.brewer.all()
 
+## ggplot2 stuff
 
+diamonds <- diamonds[sample(nrow(diamonds), 1000), ]
+diamonds$cut <- factor(diamonds$cut,
+         levels = c("Ideal", "Very Good", "Fair", "Good", "Premium"))
+
+# Repeat first example with new order
+p <- ggplot(diamonds, aes(carat, price)) +
+        geom_point()
+p + facet_grid(. ~ cut)
+p + facet_grid(color ~ cut)
+
+## reproduce Minard graph
+library(scales)
+library(ggplot2)
+cities <- read.table(url("http://www.weiwang.name/teaching/minard-cities.txt"), header=T) 
+troops <- read.table(url("http://www.weiwang.name/teaching/minard-troops.txt"), header=T) 
 plot_troops <- ggplot(troops, aes(long, lat)) +
     geom_path(aes(size = survivors, color = direction, group = group))
 
-plot_both <- troops_plot + geom_text(aes(label = city), size = 4, data = cities)
+plot_both <- plot_troops + geom_text(aes(label = city), size = 4, data = cities)
 
-plot_polished <- both +
-    scale_size(to = c(1, 10), breaks = c(1, 2, 3) * 10^5,
-               labels = comma(c(1, 2, 3) * 10^5)) +
-    scale_color_manual(values = c("grey50","red")) +
-    xlab(NULL) + ylab(NULL)
+plot_polished <- plot_both + 
+  scale_size(
+    breaks = c(1, 2, 3) * 10^5, labels = comma(c(1, 2, 3) * 10^5)) + 
+  scale_colour_manual(values = c("grey50","red")) +
+  xlab(NULL) + 
+  ylab(NULL)
