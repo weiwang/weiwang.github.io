7d5c0a9d55e81846702a28218b643a6f8d2db5a8
Wei Wang <thomas.wei.wang@gmail.com>
2015-05-28 13:28:29 -0400

    edited lec3 and demo 3

--- teaching/s1211q_2015/R_demo_3.R
+++ teaching/s1211q_2015/R_demo_3.R
@@ -2,15 +2,20 @@
 s1 = c("TH", "TT")
 s2 = c("HT", "TT")
 union(s1, s2)
-interset(s1, s2)
-setdiff(s1, s2i)
+intersect(s1, s2)
+setdiff(s1, s2)
 
 ## Law of Large Numbers
-sample(c("T", "H"), 100, replace=T)
-table(sample(c("T", "H"), 100, replace=T))/100
+N <- 10
+set.seed(1211)
+toss <- sample(c("T", "H"), N, replace=T) # sample from {T, H} N times with replacement
+sum(toss == 'H')/N # proportion of head
+
 
 ## Calculate Pi using monte carlo method
-n <- 1000
-x <- runif(n, min=-1, max=1)
-y <- runif(n, min=-1, max=1)
+N <- 100000
+set.seed(1211) 
+x <- runif(N, min=-1, max=1) # generating
+set.seed(1121)
+y <- runif(N, min=-1, max=1)
 mean((x^2+y^2)<1)*4
