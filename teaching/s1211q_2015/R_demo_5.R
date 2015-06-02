0cf1ef0c6a81dde5a16de835ca215274ed8adfee
Wei Wang <thomas.wei.wang@gmail.com>
2015-06-02 19:07:54 -0400

    modified lecs

--- teaching/s1211q_2015/R_demo_5.R
+++ teaching/s1211q_2015/R_demo_5.R
@@ -4,8 +4,12 @@ runif(4, min=0, max=1)>.98
 ## Find the probability of at least one defective tire
 N = 1000000 # number of repetitions
 n = 0 # number of occurences
+results = rep(0, N)
 for(i in 1:N){
-    if(sum(runif(4, min=0, max=1)>.98)>=1)
+    a = sum(runif(4, min=0, max=1)
+    if(sum(a>.98)>=1)
         n = n + 1
+    results = mean(a)
 }
 n/N
+
