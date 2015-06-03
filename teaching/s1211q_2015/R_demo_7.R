f27cfe3a19175bfe7f93d3fd5754ad7e0c61c681
Wei Wang <thomas.wei.wang@gmail.com>
2015-06-03 14:12:39 -0400

    some minor changes on r demo 7

--- teaching/s1211q_2015/R_demo_7.R
+++ teaching/s1211q_2015/R_demo_7.R
@@ -6,5 +6,3 @@ dbinom(x=2, size=5, prob=.6) # pmf of Bionmial
 pbinom(q=2, size=5, prob=.6) # cdf of Binomial
 qbinom(p=.5, size=5, prob=.5) # qunatile function of Binomial, here we are finding median (p=.5)
 
-## Hypergeometric
-rhyper
